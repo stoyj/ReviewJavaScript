2766024abe198795d2a36ac736eb8c8fb10131b3
stoyj <stoyanka.kamenova@gmail.com>
2025-04-03 11:51:30 +0300

    exercises

--- .vscode/Tasks with a function/tasks.js
+++ .vscode/Tasks with a function/tasks.js
@@ -20,19 +20,36 @@ console.log(multiply(2, 5));
 
 // 3️⃣ Функция за проверка дали число е положително
 // Задача: Направи функция, която казва дали дадено число е положително или отрицателно.
+// function checkNumber(num) {
+//   return num >= 0 ? "Положително" : "Отрицателно";
+// }
+
+// console.log(checkNumber(5));
+// console.log(checkNumber(-3));
+// console.log(checkNumber(0));
+
+// 4️⃣ Функция за смятане на площ на правоъгълник
+// Задача: Направи функция, която приема ширина и височина и връща площта.
+// function area(width, height) {
+//   return width * height;
+// }
+
+// console.log(area(5, 10));
+// console.log(area(3, 7));
+
+// 3️⃣ Функция за проверка дали число е положително
+// Задача: Направи функция, която казва дали дадено число е положително или отрицателно.
 function checkNumber(num) {
   return num >= 0 ? "Положително" : "Отрицателно";
 }
 
 console.log(checkNumber(5));
-console.log(checkNumber(-3));
-console.log(checkNumber(0));
+console.log(checkNumber(-8));
 
 // 4️⃣ Функция за смятане на площ на правоъгълник
 // Задача: Направи функция, която приема ширина и височина и връща площта.
 function area(width, height) {
   return width * height;
 }
-
-console.log(area(5, 10));
-console.log(area(3, 7));
+console.log(area(3, 4));
+console.log(area(5, 5));
